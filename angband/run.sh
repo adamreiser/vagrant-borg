7194fe1bbecb12c0a5309793496b1df3c6714789
Adam Reiser <reiser@defensivecomputing.io>
2016-10-27 17:55:47 -0400

    Improve autorun script

--- angband/run.sh
+++ angband/run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-while [ 1 ]; do
+# Runs the borg continuously until interrupted.
+
+# On interrupt, continues the current session
+# but does not start a new one.
+
+continue_borg=1
+
+trap "continue_borg=0" SIGINT SIGTERM
+
+while [ $continue_borg == 1 ]; do
 	~/angband-3.4.1/src/angband -b
+	clear
 done
+
+exit 0
