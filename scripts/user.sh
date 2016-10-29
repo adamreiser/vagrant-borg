8fada8a2ad1ce37878fd3fb46077914fb7070243
Adam Reiser <reiser@defensivecomputing.io>
2016-10-29 11:08:41 -0400

    Fix and document provisioning

--- scripts/user.sh
+++ scripts/user.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Copy local configs
 shopt -s dotglob
 cp /vagrant/user/.* ~/
 shopt -u dotglob
+#
 
+# Download and verify angband
 if ! -e v3.4.1.tar.gz; then
     wget https://github.com/angband/angband/archive/v3.4.1.tar.gz
 fi
@@ -11,9 +14,17 @@ fi
 if ! sha256sum -c /vagrant/angband/SHASUMS; then
     exit 1
 fi
+#
 
+# Prepare for patching
 tar -xzf v3.4.1.tar.gz
 
+if [ ! -e ~/.angband/Angband/borg.txt ]; then
+    cp angband-3.4.1/src/borg/borg.txt ~/.angband/Angband/
+fi
+#
+
+# Patch source and borg.txt
 for file in /vagrant/angband/*.patch; do
     patch -p0 < "$file"
 done
@@ -21,14 +32,11 @@ done
 for file in /vagrant/angband/borg_autorun/*.patch; do
     patch -p0 < "$file"
 done
+#
 
+# Build angband
 cd angband-3.4.1
 ./autogen.sh
 ./configure --with-no-install
 make
-
-if [ ! -e ~/.angband/Angband/borg.txt ]; then
-    cp src/borg/borg.txt ~/.angband/Angband/
-    cd ~/
-    patch -p0 < /vagrant/angband/borg.txt.patch
-fi
+#
